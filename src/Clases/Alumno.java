69d39a420d577a36548a1dffdd8437b4b7a95007
Brayan Arce <arcebrayan@gmail.com>
2025-08-29 14:59:30 -0300

    Inicio de trabajo
    -Se agregaron las clases con sus respectivas variables
    -Se agrego La interfaz principal y barra principal

---
+++ src/Clases/Alumno.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author arceb
+ */
+public class Alumno {
+    private int legajo;
+    private String apellido;
+    private String nombre;
+    
+    private HashSet<Materia> materia;
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void agregarMateria(Materia m){
+    
+    }
+    
+    //Falta el metodo cantidadMaterias
+}
